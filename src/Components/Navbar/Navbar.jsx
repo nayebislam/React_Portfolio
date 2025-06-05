8ed32d3f6d81bc0ebbd80699f15a9f24a2ba63b5
nayebislam <nayebislamnafi475@gmail.com>
2025-06-05 12:26:23 +0600

    navbar icon update

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react'
 import { BsTelephone } from "react-icons/bs";
 import { FiMail, FiSun } from "react-icons/fi";
-import { LuMoon } from "react-icons/lu";
+import { LuMail, LuMoon } from "react-icons/lu";
 import { BsWhatsapp } from "react-icons/bs";
 import { FaLinkedinIn, FaTwitter } from 'react-icons/fa6';
-import { CiInstagram } from 'react-icons/ci';
 import { IoLogoGithub } from 'react-icons/io5';
 import Conteiner from '../Conteiner/Conteiner';
+import { GrInstagram } from "react-icons/gr";
 import { HiArrowLongDown, HiArrowLongRight } from "react-icons/hi2";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
@@ -73,19 +73,19 @@ const Navbar = () => {
         </Conteiner>
         <ul className=' ml-auto space-y-6 absolute right-8'>
           <li>
-            <a href="#"><FaLinkedinIn className='w-7 h-7 text-[#b0b2c3] hover:text-white ' /></a>
+            <a href="#"><FaLinkedinIn size={35} className='text-[#b0b2c3] hover:text-white ' /></a>
           </li>
           <li>
-            <a href="#"><FaTwitter className='w-7 h-7 text-[#b0b2c3] hover:text-white ' /></a>
+            <a href="#"><FaTwitter size={35} className='text-[#b0b2c3] hover:text-white ' /></a>
           </li>
           <li>
-            <a href="#"><CiInstagram className='w-7 h-7 text-[#b0b2c3] hover:text-white ' /></a>
+            <a href=""><GrInstagram size={35} className='text-[#b0b2c3] hover:text-white' /></a>
           </li>
           <li>
-            <a href="#"><FiMail className='w-7 h-7 text-[#b0b2c3] hover:text-white ' /></a>
+            <a href="#"><LuMail size={35} className='text-[#b0b2c3] hover:text-white' /></a>
           </li>
           <li>
-            <a href="#"><IoLogoGithub className='w-7 h-7 text-[#b0b2c3] hover:text-white' /></a>
+            <a href="#"><IoLogoGithub size={35} className='text-[#b0b2c3] hover:text-white' /></a>
           </li>
         </ul>
       </div>
